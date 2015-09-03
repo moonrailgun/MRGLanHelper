f61511f7437795ca221e3e7f4bfd95470d614db6
moonrailgun <moonrailgun@gmail.com>
2015-09-03 16:47:29 +0800

    小型静态站点UI
    ADD 小型静态站点UI

---
+++ MRGLanHelper/Website.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace MRGLanHelper
+{
+    partial class Website
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("节点1");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("节点2");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("节点3");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("节点0", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3});
+            this.skinButton1 = new CCWin.SkinControl.SkinButton();
+            this.skinNumericUpDown1 = new CCWin.SkinControl.SkinNumericUpDown();
+            this.skinLabel1 = new CCWin.SkinControl.SkinLabel();
+            this.skinButton2 = new CCWin.SkinControl.SkinButton();
+            this.skinButton3 = new CCWin.SkinControl.SkinButton();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.skinTreeView1 = new CCWin.SkinControl.SkinTreeView();
+            ((System.ComponentModel.ISupportInitialize)(this.skinNumericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // skinButton1
+            // 
+            this.skinButton1.BackColor = System.Drawing.Color.Transparent;
+            this.skinButton1.ControlState = CCWin.SkinClass.ControlState.Normal;
+            this.skinButton1.DownBack = null;
+            this.skinButton1.Location = new System.Drawing.Point(10, 79);
+            this.skinButton1.MouseBack = null;
+            this.skinButton1.Name = "skinButton1";
+            this.skinButton1.NormlBack = null;
+            this.skinButton1.Size = new System.Drawing.Size(75, 23);
+            this.skinButton1.TabIndex = 0;
+            this.skinButton1.Text = "启动服务";
+            this.skinButton1.UseVisualStyleBackColor = false;
+            this.skinButton1.Click += new System.EventHandler(this.Start);
+            // 
+            // skinNumericUpDown1
+            // 
+            this.skinNumericUpDown1.Location = new System.Drawing.Point(69, 53);
+            this.skinNumericUpDown1.Maximum = new decimal(new int[] {
+            25565,
+            0,
+            0,
+            0});
+            this.skinNumericUpDown1.Minimum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.skinNumericUpDown1.Name = "skinNumericUpDown1";
+            this.skinNumericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.skinNumericUpDown1.TabIndex = 1;
+            this.skinNumericUpDown1.Value = new decimal(new int[] {
+            23333,
+            0,
+            0,
+            0});
+            // 
+            // skinLabel1
+            // 
+            this.skinLabel1.AutoSize = true;
+            this.skinLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.skinLabel1.BorderColor = System.Drawing.Color.White;
+            this.skinLabel1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.skinLabel1.Location = new System.Drawing.Point(7, 56);
+            this.skinLabel1.Name = "skinLabel1";
+            this.skinLabel1.Size = new System.Drawing.Size(56, 17);
+            this.skinLabel1.TabIndex = 2;
+            this.skinLabel1.Text = "端口号：";
+            // 
+            // skinButton2
+            // 
+            this.skinButton2.BackColor = System.Drawing.Color.Transparent;
+            this.skinButton2.ControlState = CCWin.SkinClass.ControlState.Normal;
+            this.skinButton2.DownBack = null;
+            this.skinButton2.Location = new System.Drawing.Point(10, 108);
+            this.skinButton2.MouseBack = null;
+            this.skinButton2.Name = "skinButton2";
+            this.skinButton2.NormlBack = null;
+            this.skinButton2.Size = new System.Drawing.Size(75, 23);
+            this.skinButton2.TabIndex = 3;
+            this.skinButton2.Text = "关闭服务";
+            this.skinButton2.UseVisualStyleBackColor = false;
+            this.skinButton2.Click += new System.EventHandler(this.Stop);
+            // 
+            // skinButton3
+            // 
+            this.skinButton3.BackColor = System.Drawing.Color.Transparent;
+            this.skinButton3.ControlState = CCWin.SkinClass.ControlState.Normal;
+            this.skinButton3.DownBack = null;
+            this.skinButton3.Location = new System.Drawing.Point(10, 137);
+            this.skinButton3.MouseBack = null;
+            this.skinButton3.Name = "skinButton3";
+            this.skinButton3.NormlBack = null;
+            this.skinButton3.Size = new System.Drawing.Size(75, 23);
+            this.skinButton3.TabIndex = 4;
+            this.skinButton3.Text = "查看日志";
+            this.skinButton3.UseVisualStyleBackColor = false;
+            this.skinButton3.Click += new System.EventHandler(this.ShowLog);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(8, 246);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "网站目录";
+            // 
+            // skinTreeView1
+            // 
+            this.skinTreeView1.Location = new System.Drawing.Point(92, 80);
+            this.skinTreeView1.Name = "skinTreeView1";
+            treeNode1.Name = "节点1";
+            treeNode1.Text = "节点1";
+            treeNode2.Name = "节点2";
+            treeNode2.Text = "节点2";
+            treeNode3.Name = "节点3";
+            treeNode3.Text = "节点3";
+            treeNode4.Name = "节点0";
+            treeNode4.Text = "节点0";
+            this.skinTreeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode4});
+            this.skinTreeView1.Size = new System.Drawing.Size(193, 152);
+            this.skinTreeView1.TabIndex = 6;
+            // 
+            // Website
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 263);
+            this.Controls.Add(this.skinTreeView1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.skinButton3);
+            this.Controls.Add(this.skinButton2);
+            this.Controls.Add(this.skinLabel1);
+            this.Controls.Add(this.skinNumericUpDown1);
+            this.Controls.Add(this.skinButton1);
+            this.Name = "Website";
+            this.Text = "Website";
+            ((System.ComponentModel.ISupportInitialize)(this.skinNumericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CCWin.SkinControl.SkinButton skinButton1;
+        private CCWin.SkinControl.SkinNumericUpDown skinNumericUpDown1;
+        private CCWin.SkinControl.SkinLabel skinLabel1;
+        private CCWin.SkinControl.SkinButton skinButton2;
+        private CCWin.SkinControl.SkinButton skinButton3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private CCWin.SkinControl.SkinTreeView skinTreeView1;
+    }
+}
---
+++ MRGLanHelper/Website.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CCWin;
+
+namespace MRGLanHelper
+{
+    public partial class Website : Skin_Mac
+    {
+        public Website()
+        {
+            InitializeComponent();
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Stop(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShowLog(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
